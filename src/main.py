158e1781d4fb640c2cc92f3c0ae2cd6e70098271
kians376 <kianshah376@gmail.com>
2024-01-01 02:47:19 -0800

    Removed API key

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from vast_ai import VastAPIHelper
 
 
 if __name__ == '__main__':
-    api = VastAPIHelper("5efb81d9bbc736c393b7feef35197d3646b6842c5ad4dbb3ffee83e7e4601796")
+    api = VastAPIHelper("INSERT YOUR API KEY HERE")
     api.list_available_instances()
